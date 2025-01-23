4e36c6e56b49c944cb37bac4a19b0afce482e076
Daniel Alfaro Rojas <alfaro_daniel@hotmail.com>
2025-01-23 11:08:34 -0500

    actualiza programa crear

--- orquestador/crear.py
+++ orquestador/crear.py
@@ -6,22 +6,22 @@ import sqlite3
 #print("Directorio actual:", os.getcwd())
 
 # Leer el archivo 'parametros.csv'
-df = pd.read_csv('parametros.csv', header=None)
+df = pd.read_csv('parametros.csv')
 
 # Convertir el DataFrame en un diccionario params
-params = dict(zip(df[0], df[1]))
+params = dict(zip(df['parametro'], df['valor']))
 
 # Leer el archivo con el nombre definido en base de params en un DataFrame con todas las columnas como texto
-dfbase = pd.read_excel(params.get('base') + '.xlsx', dtype=str)
-
-# Guardar el DataFrame en formato Parquet
-#dfbase.to_parquet('archivo.parquet')
+df = pd.read_excel(params.get('base') + '.xlsx', dtype=str)
+# Agregar las columnas de seguimiento 'usuario', 'fecha_inicio' y 'fecha_fin' con valores vacíos
+df[['usuario', 'robot_inicio', 'robot_fin']] = '', '', ''
+#df.to_parquet('archivo.parquet')
 
 # Conectar a la base de datos SQLite (o crearla si no existe)
 conn = sqlite3.connect('base.db')
 
 # Guardar el DataFrame en la base de datos SQLite
-dfbase.to_sql('tabla', conn, if_exists='replace', index=False)
+df.to_sql('base', conn, if_exists='replace', index=False)
 
 # Cerrar la conexión
 conn.close()
