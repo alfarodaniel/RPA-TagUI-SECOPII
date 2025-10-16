29ec685d0804f6182bdcffea522b5bfdfb1704b7
Daniel Alfaro Rojas <alfaro_daniel@hotmail.com>
2025-10-16 09:28:42 -0500

    aprobación versión 4

--- version_python/aprobacion.py
+++ version_python/aprobacion.py
@@ -18,7 +18,7 @@ redirigir_log()
 
 # Establecer variables de configuración
 variables = parametros()
-variables['robot'] = 'aprobacion_v3'
+variables['robot'] = 'aprobacion_v4'
 
 # Cargar base de datos de contratación "base_de_datos_Contratacion.xlsx" en solo texto
 dfbase = read_excel(variables['base'], dtype=str)
@@ -50,29 +50,50 @@ for i in range(0, len(dfbase)):
     mensaje(variables, 'Paso 0: Acceder al contrato --- proceso '+proceso+' - '+horainicio)
     if not acceder_contrato(r, proceso, variables): continue
 
-    # Paso 1: 8 Modificaciones del Contrato
-    print('Paso 1:  8 Modificaciones del Contrato --- proceso', proceso)
-    if not esperar(r, variables, '//*[@id="lnk_stpmStepManager9"]', 'Paso 1: Menú 8 Modificacione del Contrato'): continue
-    r.click('//*[@id="lnk_stpmStepManager9"]') # Menú 8 Modificacione del Contrato
-    if not esperar(r, variables, 'lnkEditLink_0', 'Paso 1: Enlace Editar'): continue
-    r.click('lnkEditLink_0') # Enlace Editar
-
-    # Paso 2: 1 Modificación del Contrato
-    print('Paso 2: 1 Modificación del Contrato --- proceso', proceso)
-    if not esperar(r, variables, 'IncTaskApproval_btnApproveButton', 'Paso 2: Boton Aprobar'): continue
-    r.click('IncTaskApproval_btnApproveButton') # Boton Aprobar
-    r.wait(5)
-    esperar(r, variables, '//*[@id="stepCircleSelected_1"]', 'Paso 2: Menú 1 Modificación del contrato') # Menú 1 Modificación del contrato
-    # Verificar si se encuentra el botón de Publicar modificación
-    if r.present("//input[@id='btnFinishModification' and @value='Publicar modificación']"):
-        print('Paso 2: Publicar modificación --- proceso', proceso)
-        r.click('//*[@id="btnFinishModification"]')
-        r.wait(5)
-        # Validar si aparace mensaje informativo
-        if not r.present("//div[@id='mdboxInvoicesWarningDialog' and contains(@style,'display: none')]"):
-            r.click('btnConfirmInvoicesWarningButton')
+    r.wait(10)
+    # Validar tipo de aprobación
+    if r.present('IncTaskApproval_btnApproveButton'):
+        # Paso 1: Aprobación por Proceso pendiente de aprobación/apertura
+        print('Paso 1: Aprobación por Proceso pendiente de aprobación/apertura --- proceso', proceso)
+        r.click('IncTaskApproval_btnApproveButton') # Boton Aprobar
+        r.wait(10)
+        # Validar si aparece el botón Aceptar nuevamente
+        if r.present('IncTaskApproval_btnApproveButton'):
+            r.click('IncTaskApproval_btnApproveButton') # Boton Aprobar
             r.wait(5)
+        # Validar si aparece el botón Enviar al proveedor
+        if r.present('btnSendToSupplier'):
+            r.click('btnSendToSupplier') # Enviar al proveedor
+            r.wait(5)
+    elif r.present('Enviar para aprobación ???????'):
+        # Paso 1: Aprobación por Enviar para aprobación
+        print('Paso 1: Aprobación por Enviar para aprobación --- proceso', proceso)
+        print('Aprobación por Enviar para aprobación')
+    else:
+        print('Aprobación por Modificaciones del Contrato')
+        # Paso 1: 8 Modificaciones del Contrato
+        print('Paso 1: 8 Modificaciones del Contrato --- proceso', proceso)
+        if not esperar(r, variables, '//*[@id="lnk_stpmStepManager9"]', 'Paso 1: Menú 8 Modificacione del Contrato'): continue
+        r.click('//*[@id="lnk_stpmStepManager9"]') # Menú 8 Modificacione del Contrato
+        if not esperar(r, variables, 'lnkEditLink_0', 'Paso 1: Enlace Editar'): continue
+        r.click('lnkEditLink_0') # Enlace Editar
+
+        # Paso 2: 1 Modificación del Contrato
+        print('Paso 2: 1 Modificación del Contrato --- proceso', proceso)
+        if not esperar(r, variables, 'IncTaskApproval_btnApproveButton', 'Paso 2: Boton Aprobar'): continue
+        r.click('IncTaskApproval_btnApproveButton') # Boton Aprobar
+        r.wait(5)
         esperar(r, variables, '//*[@id="stepCircleSelected_1"]', 'Paso 2: Menú 1 Modificación del contrato') # Menú 1 Modificación del contrato
+        # Verificar si se encuentra el botón de Publicar modificación
+        if r.present("//input[@id='btnFinishModification' and @value='Publicar modificación']"):
+            print('Paso 2: Publicar modificación --- proceso', proceso)
+            r.click('//*[@id="btnFinishModification"]')
+            r.wait(5)
+            # Validar si aparace mensaje informativo
+            if not r.present("//div[@id='mdboxInvoicesWarningDialog' and contains(@style,'display: none')]"):
+                r.click('btnConfirmInvoicesWarningButton')
+                r.wait(5)
+            esperar(r, variables, '//*[@id="stepCircleSelected_1"]', 'Paso 2: Menú 1 Modificación del contrato') # Menú 1 Modificación del contrato
 
     horafin = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('Terminada Aprobación del Contrato --- proceso', proceso, '-', horafin, '--------------------------------------------------')
