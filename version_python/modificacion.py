4b9ffe6c15b7a2b2473ea8d4d1ee368b7dbaeac6
Daniel Alfaro Rojas <alfaro_daniel@hotmail.com>
2026-01-22 14:54:09 -0500

    modificacion version 3

--- version_python/modificacion.py
+++ version_python/modificacion.py
@@ -3,131 +3,36 @@ Automatizar SECOP II modificación
 Modificación de información contracual en SECOP II de la información listada en el archivo "Base_de_datos_Contratacion.csv"
 """
 
+# %% Cargar datos
 # Cargar librerías
 import rpa as r
-import pandas as pd
-import re
+from pandas import read_excel
 from datetime import datetime
-import sys
+from funciones import redirigir_log, parametros, iniciar, cerrar, mensaje, esperar, acceder_contrato, anexar_documento
 
 #import os
 #os.chdir("C:\\Mis Documentos\\trabajos\\contratacion\\robots\\RPA-TagUI-SECOPII\\version_python\\")
 #print("Directorio actual:", os.getcwd())
 
 # Redirigir la salida estándar y de error a un archivo log.txt
-class Logger:
-    def __init__(self, filename):
-        self.terminal = sys.stdout
-        self.log = open(filename, 'a')
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        self.terminal.flush()
-        self.log.flush()
-
-sys.stdout = Logger('log.txt')
-sys.stderr = sys.stdout
-
-# Leer el archivo 'parametros.csv'
-print('\nCargando parametros.csv')
-df = pd.read_csv('parametros.csv')
-
-# Convertir el DataFrame en un diccionario params
-params = dict(zip(df['parametro'], df['valor']))
+redirigir_log()
 
 # Establecer variables de configuración
-repositorio = params.get('repositorio')
-robot = 'modificación_v1'
-espera = int(params.get('espera'))
-usuario = params.get('usuario')
-user = params.get('user')
-password = params.get('password')
-base = params.get('base')
-contrato = ''
-#llave = params.get('llave')
-#continuar = True
-#respositorio_base = repositorio + 'orquestador\\base.db'
-#robot_base = robot + '\\' + base + '.csv'
-#robot_exe = robot + '\\' + robot + '.exe'
+variables = parametros()
+variables['robot'] = 'modificación_v2'
 
 # Cargar base de datos de contratación "base_de_datos_Contratacion.xlsx" en solo texto
-dfbase = pd.read_excel(base, dtype=str)
-
-# Mostrar el valor de la columna 'NUMERO' para el primer registro
-#primer_registro_numero = base.loc[0, 'NUMERO']
-#print('Valor de la columna NUMERO para el primer registro:', primer_registro_numero)
-
-
-# Funcion mensaje para agregar una fila con el mensaje en los archivos historico.csv
-def mensaje(mensaje, repositorio=''):
-    #for archivo in [f"{repositorio}historico.csv", seguimiento.csv]:
-    with open(f"{repositorio}historico.csv", 'a') as file:
-        file.write(robot + ',' + 
-                usuario + ',' + 
-                contrato + ',' + 
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ',' + 
-                mensaje + '\n')
-
-
-# Función para validar si existe el registro en la página
-def esperar(objeto, descripcion="", boton="", frame="", stepCircle="", popup=""):
-    for i in range(1, espera):
-        # Verificar si depende de un boton
-        if boton != "":
-            r.click(boton)
-        # Verificar si se encuentra en un frame
-        if frame != "":
-            r.frame(frame)
-        # Verificar si se encuentra en un stepCircle
-        if stepCircle != "":
-            r.click(stepCircle)
-        # Verificar si se encuentra en un popup
-        if popup != "":
-            r.popup(popup)
-        # Verifica si se encontró objeto
-        if r.present(objeto):
-            #print('si')
-            return True  # Retorna True si el objeto fue encontrado
-        
-        print(i, 'esperando', descripcion)
-        # Esperar 1 segundo
-        r.wait(1)
-
-    print('No se encontró', descripcion)
-    # Agregar una fila con la falla en los archivos historico.csv y seguimiento.csv
-    mensaje('No se encuentra' + descripcion)
-
-    return False  # Retorna False si el objeto no fue encontrado
-
+dfbase = read_excel(variables['base'], dtype=str)
 
 # Iniciar robot
-print('Iniciar robot', robot)
+print('Iniciar robot', variables['robot'])
 r.init(visual_automation = True, turbo_mode=False)
 #r.timeout(10)
 
-# Muestra un mensaje ara garantizar que se de click dentro de la ventana para que funcione r.vision
-r.ask('Iniciar')
-
-# Arbrir INICIAR SESION
-r.url('https://community.secop.gov.co/')
-
 # Iniciar sesion
-esperar('//*[@id="btnLoginButton"]', 'Botón Iniciar Sesión')
-r.type('//*[@id="txtUserName"]', '[clear]' + user) # Celda 'Nombre de usuario'
-r.type('//*[@id="txtPassword"]', '[clear]' + password) # Celda 'Contraseña'
-r.click('//*[@id="btnLoginButton"]') # Botón 'Iniciar Sesión'
-#esperar('??????????????????????????????????????????????', 'Nombre de usuario')
-#r.click('//*[@id="btnButton1"]')
+iniciar(r, variables)
 
-# Validar si aparace mensaje informativo
-r.wait(5)
-if r.present('btnAcknowledgeGen'):
-    r.click('btnAcknowledgeGen')
-
-# Recorrer la base de datos
+# %% Recorrer la base de datos
 for i in range(0, len(dfbase)):
     # Variables
     #i=0
@@ -136,51 +41,36 @@ for i in range(0, len(dfbase)):
     # Cargar página principal
     r.url('https://community.secop.gov.co/')
 
+
     # Paso 0: Acceder al contrato
     horainicio = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('Paso 0: Acceder al contrato --- proceso',proceso,'-',horainicio,'--------------------------------------------------')
-    mensaje('Paso 0: Acceder al contrato --- proceso '+proceso+' - '+horainicio)
-    if not esperar('//*[@value="Procesos"]', 'Menú desplegable Procesos'): continue
-    r.click('//*[@value="Procesos"]') # Menú Procesos
-    if not esperar('//*[@id="lnkSubItem6"]', 'Submenú Procesos de la Entidad Estatal'): continue
-    r.click('//*[@id="lnkSubItem6"]') # Submenú Procesos de la Entidad Estatal
-    if not esperar('txtSimpleSearchInput', 'Campo Búsqueda avanzada'): continue
-    r.click('lnkAdvancedSearchLink') # Campo Búsqueda avanzada
-    #r.wait(30)
-    if not esperar('//*[@id="selFilteringStatesSel_msdd"]//*[@class="ddArrow arrowoff"]', 'Menú desplegable Mis procesos'): continue
-    r.click('//*[@id="selFilteringStatesSel_msdd"]//*[@class="ddArrow arrowoff"]') # Menú desplegable Mis procesos
-    #r.vision('type(Key.UP)') # Subir una opción a Todos
-    #r.vision('type(Key.ENTER)') # Seleccionar Todos
-    if not esperar('//*[@id="selFilteringStatesSel_child"]/ul/li[1]', 'Seleccionar Todos'): continue
-    r.click('//*[@id="selFilteringStatesSel_child"]/ul/li[1]') # Seleccionar Todos
-    #r.wait(20)
-    r.wait(2)
-    r.type('txtReferenceTextbox', '[clear]' + proceso + '[enter]') # Campo Referencia
-    #r.wait(1)
-    r.type('//*[@id="dtmbCreateDateFromBox_txt"]', '[clear]01/01/2023') # Campo Fecha de creación desde
-    #r.wait(1)
-    r.click('btnSearchButton') # Botón Buscar
-    if not esperar('//*[@title="' + proceso + '"]', 'Titulo proceso'): continue
-    r.click('//*[@title="' + proceso + '"]') # Seleccionar el proceso
-    if not esperar('incBuyerDossierDetaillnkBuyerDossierDetailLink', 'Boton Detalle'): continue
-    r.click('lnkProcurementContractViewLink_0') # Referencia
+    mensaje(variables, 'Paso 0: Acceder al contrato --- proceso '+proceso+' - '+horainicio)
+    if not acceder_contrato(r, proceso, variables): continue
+    r.wait(10)
+
 
     # Paso 1: 8 Modificaciones del Contrato
     print('Paso 1:  8 Modificaciones del Contrato --- proceso', proceso)
-    if not esperar('//*[@id="lnk_stpmStepManager9"]', 'Menú 8 Modificacione del Contrato'): continue
+    if not esperar(r, variables, '//*[@id="lnk_stpmStepManager9"]', 'Menú 8 Modificacione del Contrato'): continue
     r.click('//*[@id="lnk_stpmStepManager9"]') # Menú 8 Modificacione del Contrato
-    if not esperar('//*[@id="btnMakeModification"]', 'Botón Modificar'): continue
-    r.click('//*[@id="btnMakeModification"]') # Botón Modificar
+    if not esperar(r, variables, '//*[@id="btnMakeModification"]', 'Botón Modificar'): continue
+    # Validar si el contrato está en edición
+    if r.read('//*[@id="spnModificationStatusValue_0"]') == 'InEdition':
+        r.click('//*[@id="lnkEditLink_0"]') # Enlace Editar
+    else:
+        r.click('//*[@id="btnMakeModification"]') # Botón Modificar
     
+
     # Paso 2: 1 Modificación del Contrato
     print('Paso 2: 1 Modificación del Contrato --- proceso', proceso)
-    if not esperar('lnkModifyContractGeneralLink', 'Enlace Modificar el contrato'): continue
+    if not esperar(r, variables, 'lnkModifyContractGeneralLink', 'Enlace Modificar el contrato'): continue
     r.click('lnkModifyContractGeneralLink') # Enlace Modificar el contrato
 
     # Frame TIPO DE MODIFICACION
     #if not esperar('ProcurementContractModificationConfirmCreateTypeModal_iframe', 'Frame TIPO DE MODIFICACION'): continue
     r.frame('ProcurementContractModificationConfirmCreateTypeModal_iframe')
-    if not esperar('btnConfirmGen', 'Campo Número del proceso',frame='ProcurementContractModificationConfirmCreateTypeModal_iframe'): continue
+    if not esperar(r, variables, 'btnConfirmGen', 'Campo Número del proceso',frame='ProcurementContractModificationConfirmCreateTypeModal_iframe'): continue
     #r.click('body')
     r.click('//*[@id="chkBypassWorkflowCheck"]')
     r.wait(1)
@@ -194,43 +84,48 @@ for i in range(0, len(dfbase)):
     r.wait(5)
     r.frame()
 
+
     # Paso 3: 2 Información general
     if dfbase.loc[i, 'TIPO_MODIFICACION'] == 'ADICION - PRORROGA':
         print('Paso 3: 2 Información general --- proceso', proceso)
         r.click('stepCircle_2') # Información general
-        if not esperar('//*[@id="stepCircleSelected_2"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_2'): continue
+        if not esperar(r, variables, '//*[@id="stepCircleSelected_2"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_2'): continue
         r.type('//*[@id="dtmbContractEnd_txt"]', '[clear]' + dfbase.loc[i, 'PRORROGA_FECHA'] + ' 23:59') # Fecha de terminación del contrato
+
 
     # Paso 4: 3 Condiciones
     if dfbase.loc[i, 'TIPO_MODIFICACION'] == 'ADICION - PRORROGA':
         print('Paso 4: 3 Condiciones --- proceso', proceso)
         r.click('stepCircle_3') # Información general
-        if not esperar('//*[@id="stepCircleSelected_3"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_3'): continue
+        if not esperar(r, variables, '//*[@id="stepCircleSelected_3"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_3'): continue
         r.type('//*[@id="dtmbContractRenewalDateGen_txt"]', '[clear]' + dfbase.loc[i, 'PRORROGA_FECHA'] + ' 23:59') # Fecha de notificación de prorrogación
+
 
     # Paso 5: 4 Bienes y Servicios
     print('Paso 5: 4 Bienes y Servicios --- proceso', proceso)
     r.click('stepCircle_4') # Bienes y Servicios
-    if not esperar('//*[@id="stepCircleSelected_4"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_4'): continue
+    if not esperar(r, variables, '//*[@id="stepCircleSelected_4"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_4'): continue
     r.click('//*[@id="incCatalogueItemsfltDataSheet"]/table/tbody/tr[2]/td[4]') # Símbolo + en Incluya el precio como lo indique la Entidad Estatal
-    if not esperar('//*[@id="incCatalogueItemsfltDataSheet"]/table/tbody/tr[5]/td[5]/table/tbody/tr/td/table/tbody/tr[1]/td[7]/input', 'Campo Precio unitario'): continue
+    if not esperar(r, variables, '//*[@id="incCatalogueItemsfltDataSheet"]/table/tbody/tr[5]/td[5]/table/tbody/tr/td/table/tbody/tr[1]/td[7]/input', 'Campo Precio unitario'): continue
     r.dclick('//*[@id="incCatalogueItemsfltDataSheet"]/table/tbody/tr[5]/td[5]/table/tbody/tr/td/table/tbody/tr[1]/td[7]/input') # Campo Precio unitario
     r.type('//*[@id="incCatalogueItemsfltDataSheet"]/table/tbody/tr[5]/td[5]/table/tbody/tr/td/table/tbody/tr[1]/td[7]/input', dfbase.loc[i, 'VALOR TOTAL']) # Campo Precio unitario
+
 
     # Paso 6: 7 Informacion presupuestal
     print('Paso 6: 7 Información presupuestal --- proceso', proceso)
     r.click('stepCircle_7') # Informacion presupuestal
-    if not esperar('//*[@id="stepCircleSelected_7"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_7'): continue
-    if not esperar('cbxOwnResourcesAGRIValue', 'Campo Recursos Propios'): continue
+    if not esperar(r, variables, '//*[@id="stepCircleSelected_7"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro',stepCircle='stepCircle_7'): continue
+    if not esperar(r, variables, 'cbxOwnResourcesAGRIValue', 'Campo Recursos Propios'): continue
     r.dclick('cbxOwnResourcesAGRIValue') # Campo Recursos Propios
     r.type('cbxOwnResourcesAGRIValue', dfbase.loc[i, 'VALOR TOTAL']) # Campo Recursos Propios
-    if not esperar('//*[@id="SIIFModal_iframe"]', 'Frame Información presupuestal',boton='btnAddCode'): continue
+    if not esperar(r, variables, '//*[@id="SIIFModal_iframe"]', 'Frame Información presupuestal',boton='btnAddCode'): continue
     #r.click('btnAddCode') # Botón Agregar de CDP/Vigencias Futuras
 
     # Frame Información presupuestal
     r.frame('SIIFModal_iframe')
     #esperar('//*[@id="rdbgOptionsToSelectRadioButton_0"]', 'Radio button CDP',frame='SIIFModal_iframe')
-    if not esperar('//*[@id="rdbgOptionsToSelectRadioButton_0"]', 'Radio button CDP',frame='SIIFModal_iframe'): continue
+    if not esperar(r, variables, '//*[@id="rdbgOptionsToSelectRadioButton_0"]', 'Radio button CDP',frame='SIIFModal_iframe'): continue
+    #r.wait(3)
     r.type('//*[@id="rdbgOptionsToSelectRadioButton_0"]', 'Yes') # Radio button CDP
     r.vision('type(Key.SPACE)') # Radio button CDP
     r.type('txtSIIFIntegrationItemTextbox', '[clear]') # Campo Código 
@@ -243,40 +138,24 @@ for i in range(0, len(dfbase)):
     r.vision('type(Key.SPACE)') # Botón Crear
     r.frame()
     
+
     # Paso 7: 1 Modificación del Contrato
     print('Paso 7: 1 Modificación del Contrato --- proceso', proceso)
     r.click('stepCircle_1') # Menù 1 Modificación del Contrato
-    if not esperar('//*[@id="stepCircleSelected_1"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro', stepCircle='stepCircle_1'): continue
+    if not esperar(r, variables, '//*[@id="stepCircleSelected_1"][@class="MainColor4 circle22 Black stepOn"]', 'stepCircle Configuracion en negro', stepCircle='stepCircle_1'): continue
     r.wait(2)
     r.click('cmAttachmentsOptions') # Lista Anexar documentos
     r.wait(2)
     r.click('linkUploadNew') # Item Anexar nuevo documento
-    #r.vision('type(Key.TAB)')
-    #r.vision('type(Key.ENTER)')
-    #r.wait(5)
-    
-    # Popup ANEXAR DOCUMENTO
-    r.popup('DocumentAlternateUpload')
-    #esperar('divAddFilesButton', 'Boton Buscar documento', popup='DocumentAlternateUpload')
-    if not esperar('divAddFilesButton', 'Boton Buscar documento', popup='DocumentAlternateUpload'): continue
-    r.click('divAddFilesButton') # Boton Buscar documento
-    r.wait(5)
-    rutaarchivo = re.sub(r'\\+', r'\\', f'{repositorio}documentos\\{dfbase.loc[i, "NOMBRE_DOCUMENTO"]}.zip')
-    r.vision(f'type("{rutaarchivo}")') # Ruta del documento
-    r.vision('type(Key.ENTER)')
-    r.wait(5)
-    r.click('btnUploadFilesButtonBottom') # Botón Anexar
-    if not esperar('//*[@id="tblFilesTable"]//*[@processed="success"]', 'Progreso DOCUMENTO ANEXO'): continue
-    r.click('btnCancelBottomButtom') # Botón Cerrar
-    r.popup(None) # Cierra el contexto del popup
-    r.wait(5)
+    r.wait(3)
+    anexar_documento(r, variables, dfbase.loc[i, "NOMBRE_DOCUMENTO"]) # Popup ANEXAR DOCUMENTO
     
     print('--- Finalizar Modificacion --- proceso', proceso)
     r.click('body')
-    if not esperar('txaModificationPurpose', 'Campo Justificación de la modificación'): continue
+    if not esperar(r, variables, 'txaModificationPurpose', 'Campo Justificación de la modificación'): continue
     r.type('txaModificationPurpose', 'Modificación') # Campo Justificación de la modificación
     r.click('btnOption_tbContractToolbar_Finish') # Finalizar Modificacion
-    if not esperar('chkCheckBoxAgreeTerms', 'Check box Acepto el valor del contrato'): continue
+    if not esperar(r, variables, 'chkCheckBoxAgreeTerms', 'Check box Acepto el valor del contrato'): continue
     #r.type('chkCheckBoxAgreeTerms', 'yes') # Check box Acepto el valor del contrato
     r.click('chkCheckBoxAgreeTerms') # Check box Acepto el valor del contrato
     r.wait(2)
@@ -289,7 +168,7 @@ for i in range(0, len(dfbase)):
 
     # Frame Confirmar
     r.frame('StartApprovalSupportModal_iframe')
-    if not esperar('//*[@id="btnConfirmGen"]', 'Boton Confirmar',frame='StartApprovalSupportModal_iframe'): continue
+    if not esperar(r, variables, '//*[@id="btnConfirmGen"]', 'Boton Confirmar',frame='StartApprovalSupportModal_iframe'): continue
     r.type('//*[@id="btnConfirmGen"]', 'Yes') # Boton Confirmar
     #r.wait(2)
     #r.type('//*[@id="btnCancelGen"]', 'Yes') # Boton Cancelar
@@ -301,15 +180,11 @@ for i in range(0, len(dfbase)):
     horafin = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('Terminada Modificación del Contrato --- proceso', proceso, '-', horafin, '--------------------------------------------------')
     mensaje('Terminada Modificación del Contrato --- proceso '+proceso+' - inicio: '+horainicio+' - fin: '+horafin)
-    mensaje('Modificación del Contrato --- proceso '+proceso+' - inicio: '+horainicio+' - fin: '+horafin, repositorio)
-
+    mensaje('Modificación del Contrato --- proceso '+proceso+' - inicio: '+horainicio+' - fin: '+horafin, variables['repositorio'])
 
 # Cerrar sesión
-print('Cerrar sesion')
-r.click('//*[@id="userImage"]/img') # Imagen usuario
-r.wait(1)
-r.click('//*[@id="logOut"]') # Opción Salir
+cerrar(r)
 
 # Cerrar robot
-print('Cerrar robot', robot)
+print('Cerrar robot', variables['robot'])
 r.close()
